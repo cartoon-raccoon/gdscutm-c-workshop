1229d0f8b3d91fb205820a28c972edd7d6b2b550
cartoon-raccoon <shaughn.chan@gmail.com>
2023-02-23 01:46:29 -0500

    Updated to correct code

--- linkedlist.c
+++ linkedlist.c
@@ -8,40 +8,43 @@ void populate_list(linkedlist *list);
 // driver code
 int main(void)
 {
-    linkedlist list;
-    list.head = NULL;
+    linkedlist *list = linkedlist_create();
     
     // test printing on an empty list
-    linkedlist_print(&list);
+    linkedlist_print(list);
 
     // populate the list with numbers
-    populate_list(&list);
+    populate_list(list);
 
     // print the new list
-    linkedlist_print(&list);
+    linkedlist_print(list);
 
     // try removing an item from the list
-    int item1 = linkedlist_remove(&list, 4);
+    int item1 = linkedlist_remove(list, 4);
     printf("Removed item at index 4, got %i\n", item1);
 
     // try removing from an out of bounds index
-    int item2 = linkedlist_remove(&list, 10);
+    int item2 = linkedlist_remove(list, 10);
     printf("Out of bounds was handled, got %i\n", item2);
 
     // try and out of bounds index
-    int *idx = linkedlist_index(&list, 10);
-    if (idx == NULL) {
+    int *idx = linkedlist_index(list, 10);
+    if (idx == NULL)
+    {
         printf("Out of bounds access was handled correctly!\n");
     }
 
-    linkedlist_print(&list);
+    linkedlist_print(list);
+    linkedlist_destroy(list);
+    list = NULL;
 
     return 0;
 }
 
 void populate_list(linkedlist *list)
 {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         linkedlist_push(list, i + 1);
     }
 
